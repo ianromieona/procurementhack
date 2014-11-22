03b676ff4c993b48bdb0ebab0f36dde32c753e42
Edbert Guinto <ecg_24k@yahoo.com>
2014-11-22 15:06:59 +0800

    test the api of philgeps

--- protected/models/PhilgepsApi.php
+++ protected/models/PhilgepsApi.php
@@ -6,8 +6,18 @@
 
 class PhilgepsApi extends CFormModel
 {
-	public static function listPhilgepsData($table,$data = '*',$condition){
-		$url = "http://philgeps.cloudapp.net:5000/api/action/datastore_search_sql?sql=SELECT ".$data." from '".$table."' ".$condition;
+	public static function listPhilgepsData($table,$data = '*',$condition = ""){
+
+		// guide for the $table variable
+		// award 		 =	539525df-fc9a-4adf-b33d-04747e95f120
+		// bidders list	 = 	6427affb-e841-45b8-b0dc-ed267498724a
+		// organization	 =	ec10e1c4-4eb3-4f29-97fe-f09ea950cdf1
+		// bid line item =	daa80cd8-da5d-4b9d-bb6d-217a360ff7c1
+		// bind info 	 = 	baccd784-45a2-4c0c-82a6-61694cd68c9d
+		// project loc   = 	116b0812-23b4-4a92-afcc-1030a0433108
+		// org business	 = 	58ea40bf-15e9-4c38-adef-fd93455d8c7e
+
+		$url = str_replace(' ', '%20',"http://philgeps.cloudapp.net:5000/api/action/datastore_search_sql?sql=SELECT ".$data." from \"".$table."\" ".$condition);
 		
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -17,5 +27,6 @@ class PhilgepsApi extends CFormModel
 		$result = json_decode(curl_exec($ch),TRUE);
 		curl_close($ch);
 		return $result;
+
 	}
 }
