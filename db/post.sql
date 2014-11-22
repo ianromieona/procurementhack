5587ff0d80275d1dd46de198dc845b48061b9fe6
Ariel Moris <ariel.s.moris@gmail.com>
2014-11-23 00:47:06 +0800

    post table

--- db/post.sql
+++ db/post.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2014 at 04:37 PM
+-- Generation Time: Nov 22, 2014 at 05:45 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
