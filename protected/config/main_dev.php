76b1a91c029f260c5aa7934757720e9d449c72b8
rienierpatron <rienierpatron@gmail.com>
2014-11-22 17:02:49 +0800

    layouting

--- protected/config/main_dev.php
+++ protected/config/main_dev.php
@@ -55,7 +55,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=procurement',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
