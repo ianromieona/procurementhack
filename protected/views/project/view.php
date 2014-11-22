199adee5bf75933031c8a44e28671c4690f1cf27
ianromie <ianromie@gmail.com>
2014-11-22 06:38:50 -0500

    project

--- protected/views/project/view.php
+++ protected/views/project/view.php
@@ -1,10 +1,18 @@
+<div id="banner">
+	<div class="container intro_wrapper">
+		<div class="inner_content">
+			<h1>Bid Notice Abstract</h1>
+			<h1 class="title">Request for Quotation (RFQ)</h1>
+		</div>
+	</div>
+</div>
 <div class="semicontainer">
 	<div class="row-fluid">
 		<div class="span2">
 			<label class="labels">Reference Number</label>
 		</div>
 		<div class="span2">
-			<label class="labels2">2943299</label>
+			<label class="labels2 text-info">2943299</label>
 		</div>
 	</div>
 	<div class="row-fluid">
@@ -12,7 +20,7 @@
 			<label class="labels">Procuring Entity</label>
 		</div>
 		<div class="span10">
-			<label class="labels2">DEPARTMENT OF AGRARIAN REFORM - ILOILO PROVINCIAL OFFICE</label>
+			<label class="labels2 text-error">DEPARTMENT OF AGRARIAN REFORM - ILOILO PROVINCIAL OFFICE</label>
 		</div>
 	</div>
 	<div class="row-fluid">
@@ -159,6 +167,96 @@
 					<label class="labels2">22/11/2014</label>
 				</div>
 			</div>
+		</div>
+	</div>
+		<hr>
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="span12">
+				<h2><span>Description</span></h2>
+				<h5>Publication of Master list of ARB's</h5>
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<div class="span12">
+				<h3><span>Line Items</span></h3>
+			</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span12">
+			<table class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>Item No.</th>
+                        <th>Product/Service Name</th>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>UOM</th>
+                        <th>Budget (PHP)</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Smartphones</td>
+                        <td>480px and below</td>
+                        <td >Fluid columns, no fixed widths</td>
+                        <td >Fluid columns, no fixed widths</td>
+                        <td >Fluid columns, no fixed widths</td>
+                        <td >Fluid columns, no fixed widths</td>
+                      </tr>
+                      <tr>
+                        <td>Smartphones to tablets</td>
+                        <td>767px and below</td>
+                        <td >Fluid columns, no fixed widths</td>
+                        <td >Fluid columns, no fixed widths</td>
+                        <td >Fluid columns, no fixed widths</td>
+                        <td >Fluid columns, no fixed widths</td>
+                      </tr>
+                      <tr>
+                        <td>Portrait tablets</td>
+                        <td>768px and above</td>
+                        <td>42px</td>
+                        <td>20px</td>
+                        <td>20px</td>
+                        <td>20px</td>
+                      </tr>
+                      <tr>
+                        <td>Default</td>
+                        <td>980px and up</td>
+                        <td>60px</td>
+                        <td>20px</td>
+                        <td>20px</td>
+                        <td>20px</td>
+                      </tr>
+                      <tr>
+                        <td>Large display</td>
+                        <td>1200px and up</td>
+                        <td>70px</td>
+                        <td>30px</td>
+                        <td>30px</td>
+                        <td>30px</td>
+                      </tr>
+                    </tbody>
+                  </table>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span2">
+			<label class="labels">Created By</label>
+		</div>
+		<div class="span2">
+			<label class="labels2">2943299</label>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span2">
+			<label class="labels">Date Created</label>
+		</div>
+		<div class="span2">
+			<label class="labels2">2943299</label>
 		</div>
 	</div>
 </div>
